38762f90a9bfea5bfc22f53cb685629eca8f3a62
Rashotik <95523846+Rashotik@users.noreply.github.com>
2022-02-09 14:12:23 +0300

    Update UserDaoHibernateImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,22 +4,41 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
+import org.hibernate.query.Query;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    private UserDao jdbc = new UserDaoJDBCImpl();
 
     public UserDaoHibernateImpl() {
 
     }
 
     public void createUsersTable() {
-        jdbc.createUsersTable();
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        String sql = "CREATE TABLE IF NOT EXISTS `user` " +
+                "(`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "`name` VARCHAR(50) NOT NULL, " +
+                "`lastName` VARCHAR(50) NOT NULL, " +
+                "`age` INT NOT NULL)";
+
+        Query query = session.createSQLQuery(sql).addEntity(User.class);
+        query.executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     public void dropUsersTable() {
-        jdbc.dropUsersTable();
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        String sql = "DROP TABLE IF EXISTS user";
+
+        Query query = session.createSQLQuery(sql).addEntity(User.class);
+        query.executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     public void saveUser(String name, String lastName, byte age) {
